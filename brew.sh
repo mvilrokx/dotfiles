89a79291fc365c4c142be428df06c84357b28d96
mvilrokx <mvilrokx@gmail.com>
2022-10-26 21:24:26 -0700

    Update brew
    I used brew.sh ond a brand new Mac and it raised a bunch of errors and
    warnings. This commit is an attempt to fix those.

--- brew.sh
+++ brew.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install command-line tools using Homebrew.
-''
+
 # Make sure we’re using the latest Homebrew.
 brew update
 
@@ -21,10 +21,10 @@ brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
+brew install gnu-sed
 # Install a modern version of Bash.
 brew install bash
-brew install bash-completion2
+brew install bash-completion@2
 
 # Switch to using brew-installed bash as default shell
 #if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
@@ -33,22 +33,22 @@ brew install bash-completion2
 #fi;
 
 # Install `wget` with IRI support.
-brew install wget --with-iri
+brew install wget
 
 # Install GnuPG to enable PGP-signing commits.
 brew install gnupg
 
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
+brew install vim
 brew install grep
 brew install openssh
 brew install screen
 
 # Install fonts
+brew install svn
 brew tap homebrew/cask-fonts
 brew install --cask font-clear-sans
 brew install --cask font-hack-nerd-font
-brew install --cask font-m-plus
 brew install --cask font-roboto
 brew install --cask font-source-code-pro
 
@@ -58,8 +58,10 @@ brew install logitech-options
 
 # Install other useful binaries.
 brew install ack
+brew install ansible
 brew install automake
 brew install brew-cask-completion
+brew install dockutil
 brew install gawk
 brew install git
 brew install git-lfs
@@ -69,9 +71,9 @@ brew install grep
 brew install help2man
 brew install httpie
 brew install hub
-brew install imagemagick --with-webp
+brew install imagemagick
 brew install jq
-brew install kafkacat
+brew install kcat
 brew install libtool
 brew install mackup
 brew install maven
@@ -85,7 +87,8 @@ brew install pv
 brew install pyenv
 brew install rename
 brew install ssh-copy-id
-brew install https://raw.githubusercontent.com/kadwanev/bigboybrew/master/Library/Formula/sshpass.rb
+# not supported
+# brew install https://raw.githubusercontent.com/kadwanev/bigboybrew/master/Library/Formula/sshpass.rb
 brew install telnet
 brew install tmux
 brew install trash
@@ -95,14 +98,15 @@ brew install ykman
 
 # Install Applications.
 brew install --cask 1password
-brew install --cask adoptopenjdk
+brew install --cask temurin
 brew install --cask alfred
 brew install --cask bettertouchtool
 brew install --cask caffeine
 brew install --cask dash
 brew install --cask dbeaver-community
 brew install --cask devtoys
-brew install --cask docker
+# conflicts with rancher so choose this or rancher
+# brew install --cask docker
 brew install --cask drawio
 brew install --cask dropbox
 brew install --cask firefox
@@ -123,7 +127,7 @@ brew install --cask virtualbox
 brew install --cask visual-studio-code
 brew install --cask vlc
 brew install --cask xquartz
-brew install --cask zoomus
+brew install --cask zoom
 
 # Install apps from the App Store
 # requires mas, which is installed in the above brews
